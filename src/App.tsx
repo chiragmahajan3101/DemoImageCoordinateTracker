660329f89902230a6dfabd7d604cff4d9bfd3a08
Chirag Mahajan <chiragmahajan3101@gmail.com>
2026-09-10 00:42:04 -0500

    Created a basic react app using vite for quick starter and using tsx format

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
