dbea67b3df55da5d1fb0705492ed5288053d1f73
Chirag Mahajan <chiragmahajan3101@gmail.com>
2026-09-10 00:54:56 -0500

    added react-router-dom to enable routing through the entire app using different endpoints

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id="center">
+      {/* <section id="center">
         <div>
           <h1>Get started</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
-      </section>
+      </section> */}
+      <Routes>
+        <Route path="/" element={<CoordinateTracker />}>
+      </Routes>
     </>
   )
 }
